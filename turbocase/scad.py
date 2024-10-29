54761d67de9396d5102c4e0f0b68cf03791f1f32
Clement Roblot <clement.roblot@martobre.fr>
2024-10-29 14:34:15 +0700

    Make use of the template

--- turbocase/scad.py
+++ turbocase/scad.py
@@ -1,77 +1,5 @@
 import logging
 
-_template = """
-module wall (thickness, height) {
-    linear_extrude(height, convexity=10) {
-        difference() {
-            offset(r=thickness)
-                children();
-            children();
-        }
-    }
-}
-
-module bottom(thickness, height) {
-    linear_extrude(height, convexity=3) {
-        offset(r=thickness)
-            children();
-    }
-}
-
-module lid(thickness, height, edge) {
-    linear_extrude(height, convexity=10) {
-        offset(r=thickness)
-            children();
-    }
-    translate([0,0,-edge])
-    difference() {
-        linear_extrude(edge, convexity=10) {
-                offset(r=-0.2)
-                children();
-        }
-        translate([0,0, -0.5])
-         linear_extrude(edge+1, convexity=10) {
-                offset(r=-1.2)
-                children();
-        }
-    }
-}
-
-
-module box(wall_thick, bottom_layers, height) {
-    if (render == "all" || render == "case") {
-        translate([0,0, bottom_layers])
-            wall(wall_thick, height) children();
-        bottom(wall_thick, bottom_layers) children();
-    }
-    
-    if (render == "all" || render == "lid") {
-        translate([0, 0, height+bottom_layers+0.1])
-        lid(wall_thick, bottom_layers, lid_model == "inner-fit" ? headroom-2.5: bottom_layers) 
-            children();
-    }
-}
-
-module mount(drill, space, height) {
-    translate([0,0,height/2])
-        difference() {
-            cylinder(h=height, r=(space/2), center=true);
-            cylinder(h=(height*2), r=(drill/2), center=true);
-            
-            translate([0, 0, height/2+0.01])
-                children();
-        }
-        
-}
-
-module connector(min_x, min_y, max_x, max_y, height) {
-    size_x = max_x - min_x;
-    size_y = max_y - min_y;
-    translate([(min_x + max_x)/2, (min_y + max_y)/2, height/2])
-        cube([size_x, size_y, height], center=true);
-}
-"""
-
 
 def esc(inp):
     return inp.replace('.', '_')
@@ -209,7 +137,8 @@ def generate(case, show_pcb=False):
     result += f'inner_height = floor_height + standoff_height + pcb_thickness + headroom;\n'
     result += '\n'
 
-    result += _template.lstrip() + "\n"
+    with open("turbocase/templates/baseBox.scad", "r") as file:
+        result += file.read().lstrip() + "\n"
 
     for m in case.modules:
         result += m + "\n"
